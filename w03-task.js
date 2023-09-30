39ee99a3faaf09331f09391d432d15f180c552c3
TheTubbyRaider <grsraider66@gmail.com>
2023-09-30 14:19:24 -0600

    Week 3 tasks

--- w03-task.js
+++ w03-task.js
@@ -1,73 +1,71 @@
 function add(number1, number2) {
-    return number1 + number2;
-  }
-  
-  function addNumbers() {
-    let addNumber1 = Number(document.querySelector('#add1').value);
-    let addNumber2 = Number(document.querySelector('#add2').value);
-    document.querySelector('#sum').textContent = add(addNumber1, addNumber2);
-  }
-  
-  document.querySelector('#addNumbers').addEventListener('click', addNumbers);
-  
-  // Subtraction Feature
-  const subtract = function (number1, number2) {
-    return number1 - number2;
-  };
-  
-  const subtractNumbers = function () {
-    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
-    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
-    document.querySelector('#difference').textContent = subtract(subtractNumber1, subtractNumber2);
-  };
-  
-  document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
-  
-  // Multiplication Feature
-  const multiply = (number1, number2) => number1 * number2;
-  
-  const multiplyNumbers = () => {
-    let factor1 = Number(document.querySelector('#factor1').value);
-    let factor2 = Number(document.querySelector('#factor2').value);
-    document.querySelector('#product').textContent = multiply(factor1, factor2);
-  };
-  
-  document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
-  
-  // Division Feature
-  function divide(number1, number2) {
-    return number1 / number2;
-  }
-  
-  function divideNumbers() {
-    let dividend = Number(document.querySelector('#dividend').value);
-    let divisor = Number(document.querySelector('#divisor').value);
-    document.querySelector('#quotient').textContent = divide(dividend, divisor);
-  }
-  
-  document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
-  
-  // Get Current Year
-  let currentDate = new Date();
-  let currentYear = currentDate.getFullYear();
-  document.querySelector('#year').textContent = currentYear;
-  
-  // Array Methods - Functional Programming
-  let numbersArray = Array.from({ length: 13 }, (_, index) => index + 1);
-  
-  document.querySelector('#array').textContent = numbersArray;
-  
-  let oddNumbers = numbersArray.filter(number => number % 2 === 1);
-  document.querySelector('#odds').textContent = oddNumbers;
-  
-  let evenNumbers = numbersArray.filter(number => number % 2 === 0);
-  document.querySelector('#evens').textContent = evenNumbers;
-  
-  let sumOfArray = numbersArray.reduce((sum, number) => sum + number);
-  document.querySelector('#sumOfArray').textContent = sumOfArray;
-  
-  let multiplied = numbersArray.map(number => number * 2);
-  document.querySelector('#multiplied').textContent = multiplied;
-  
-  let sumOfMultiplied = multiplied.reduce((sum, number) => sum + number);
-  document.querySelector('#sumOfMultiplied').textContent = sumOfMultiplied;
+  return number1 + number2;
+}
+
+function addNumbers() {
+  const add1Value = parseFloat(document.getElementById('add1').value);
+  const add2Value = parseFloat(document.getElementById('add2').value);
+  const sumValue = add(add1Value, add2Value);
+  document.getElementById('sum').textContent = sumValue;
+}
+
+document.getElementById('addNumbers').addEventListener('click', addNumbers);
+
+const subtract = function (number1, number2) {
+  return number1 - number2;
+};
+
+const subtractNumbers = function () {
+  const subtract1Value = parseFloat(document.getElementById('subtract1').value);
+  const subtract2Value = parseFloat(document.getElementById('subtract2').value);
+  const differenceValue = subtract(subtract1Value, subtract2Value);
+  document.getElementById('difference').textContent = differenceValue;
+};
+
+document.getElementById('subtractNumbers').addEventListener('click', subtractNumbers);
+
+const multiply = (number1, number2) => number1 * number2;
+
+const multiplyNumbers = () => {
+  const factor1Value = parseFloat(document.getElementById('factor1').value);
+  const factor2Value = parseFloat(document.getElementById('factor2').value);
+  const productValue = multiply(factor1Value, factor2Value);
+  document.getElementById('product').textContent = productValue;
+};
+
+document.getElementById('multiplyNumbers').addEventListener('click', multiplyNumbers);
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function divideNumbers() {
+  const dividendValue = parseFloat(document.getElementById('dividend').value);
+  const divisorValue = parseFloat(document.getElementById('divisor').value);
+  const quotientValue = divide(dividendValue, divisorValue);
+  document.getElementById('quotient').textContent = quotientValue;
+}
+
+document.getElementById('divideNumbers').addEventListener('click', divideNumbers);
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+document.getElementById('year').textContent = currentYear;
+
+const numbersArray = [...Array(13).keys()].map((x) => x + 1);
+document.getElementById('array').textContent = numbersArray.join(', ');
+
+const oddNumbers = numbersArray.filter((number) => number % 2 === 1);
+document.getElementById('odds').textContent = oddNumbers.join(', ');
+
+const evenNumbers = numbersArray.filter((number) => number % 2 === 0);
+document.getElementById('evens').textContent = evenNumbers.join(', ');
+
+const sumOfArray = numbersArray.reduce((acc, number) => acc + number, 0);
+document.getElementById('sumOfArray').textContent = sumOfArray;
+
+const multipliedArray = numbersArray.map((number) => number * 2);
+document.getElementById('multiplied').textContent = multipliedArray.join(', ');
+
+const sumOfMultiplied = multipliedArray.reduce((acc, number) => acc + number, 0);
+document.getElementById('sumOfMultiplied').textContent = sumOfMultiplied;
